093a4b4f548ad553dfef00a4bab2368a72503f15
Agent-Shu <77569575+Agent-Shu@users.noreply.github.com>
2023-04-11 22:51:51 +0530

    .

---
+++ main.js
@@ -0,0 +1,29 @@
+function cssandjschange() {
+    let ua = navigator.userAgent.toLowerCase();
+	let isMobile = ua.indexOf("mobile") !== -1 || ua.indexOf("android") !== -1 || ua.indexOf("iphone") !== -1 || ua.indexOf("ipad") !== -1;
+
+    if(isMobile === true){
+        const script = document.createElement('script');
+        script.id = 'mobile_script';
+        script.src = 'mobile_script.js';
+        document.head.appendChild(script);
+
+        document.getElementById('css_sheet').href="mobile_style.css";
+    }
+
+    else {
+        const script = document.createElement('script');
+        script.id = 'pc_script';
+        script.src = 'pc_script.js';
+        document.head.appendChild(script);
+
+        document.getElementById('css_sheet').href="pc_style.css";
+    }
+
+}
+
+cssandjschange();
+
+
+
+
