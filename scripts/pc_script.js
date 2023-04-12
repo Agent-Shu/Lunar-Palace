f227b0a149f215eb814cd8ab1e62cad01f385b71
Agent-Shu <77569575+Agent-Shu@users.noreply.github.com>
2023-04-13 00:51:40 +0530

    .

--- scripts/pc_script.js
+++ scripts/pc_script.js
@@ -1,3 +1,17 @@
+let x = 0;
+
+fetch('./data/data.json')
+
+.then(response => response.json())
+
+.then((json) => {
+    
+    x = json.filter((item) => { return item.id == 2;});
+
+    if(x[0] != null)
+        console.log(x);
+ 
+});
 
 
 
@@ -8,3 +22,24 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(json);
+
+// console.log(json.filter((item) => { return item.id == 2;}));
