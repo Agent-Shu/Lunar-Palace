fb7ebd753dbe787a0efa6a8cf31cced882c3984c
Agent-Shu <77569575+Agent-Shu@users.noreply.github.com>
2023-04-14 03:12:20 +0530

    .

--- scripts/pc_script.js
+++ scripts/pc_script.js
@@ -1,16 +1,17 @@
 let x = 0;
 
 fetch('./data/data.json')
-
 .then(response => response.json())
+.then(json => { 
 
-.then((json) => {
     
-    x = json.filter((item) => { return item.id == 2;});
+    x = json.filter((item) => { return item.name == "Albedo";});
 
-    if(x[0] != null)
-        console.log(x);
- 
+    // if(x[0] != null)
+        // console.log(x[0].img[0]);
+
+    // document.getElementsByClassName("image")[0].src = x[0].img[0];
+
 });
 
 
@@ -18,6 +19,29 @@ fetch('./data/data.json')
 
 
 
+
+
+
+
+
+let ch = 0;
+const temp = setInterval(() =>{
+    ch = Math.floor(Math.random() * 4);
+    console.log(ch);
+
+    if(ch == 0)
+        document.getElementsByClassName("home_page_image")[0].src = "./Images/Home_Page/Monstadt.jpg" ;
+
+    if(ch == 1)
+        document.getElementsByClassName("home_page_image")[0].src = "./Images/Home_Page/Liyue.png" ;
+        
+    if(ch == 2)
+        document.getElementsByClassName("home_page_image")[0].src = "./Images/Home_Page/Inazuma.png" ;
+
+    if(ch == 3)
+        document.getElementsByClassName("home_page_image")[0].src = "./Images/Home_Page/Sumeru.jpg" ;
+   
+}, 3000);
 
 
 
@@ -43,3 +67,6 @@ fetch('./data/data.json')
 // console.log(json);
 
 // console.log(json.filter((item) => { return item.id == 2;}));
+
+
+
