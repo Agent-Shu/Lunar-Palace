96f77484c79ca7a22b78bf3d4ef7948358e5cd98
Agent-Shu <77569575+Agent-Shu@users.noreply.github.com>
2023-04-21 02:32:23 +0530

    dynamic char scroll

--- scripts/pc_script.js
+++ scripts/pc_script.js
@@ -22,10 +22,8 @@ function goto_character(character){
 }
 
 
-function character_select()
+function character_select(character)
 {
-
-
 
 
     
@@ -67,29 +65,54 @@ function character_select()
 
 
 
-fetch('./data/data.json')
-.then(response => response.json())
-.then(json => { 
-    let x = json.filter((item) => { return item.name == "Amber";});
-
-    if(x[0] != null)
-        
-        console.log(x[0].build_type);
-
-
-    for (let key in x[0].build_type)
-        console.log(key);
 
 
 
 
 
-});
 
 
 
 
 
+
+
+
+
+
+function character_page_scroll_element(){
+
+    let max_char=0;
+
+    fetch('./data/data.json')
+    .then(response => response.json())
+    .then(json => { 
+        let x = json.filter((item) => {return item});
+
+        for(let num in x)
+            max_char = num;
+
+        for(let i=0 ; i <= max_char ; i++){
+
+            let div = document.createElement("div");
+            div.className="charcter_scroller_item";
+            div.setAttribute("type","submit");
+            div.setAttribute("onclick",`character_select("${x[i].name}")`);
+            document.getElementsByClassName("charcter_scroller")[0].appendChild(div);
+
+            let div1 = document.createElement("img");
+            div1.className="scroller_img";
+            div1.setAttribute("src",`${x[i].img[0]}`)
+            document.getElementsByClassName("charcter_scroller_item")[i].appendChild(div1);
+
+            let spa = document.createElement("span");
+            spa.className="tooltip";
+            spa.innerHTML=`${x[i].name}`;
+            document.getElementsByClassName("charcter_scroller_item")[i].appendChild(spa);
+        }
+    });
+}
+character_page_scroll_element();
 
 
 const images = [
@@ -98,7 +121,6 @@ const images = [
   "./Images/Home_Page/Inazuma.png",
   "./Images/Home_Page/Sumeru.jpg"
 ];
-
 function bg_change(){
     const temp = setInterval(() =>{
         document.getElementsByClassName("background_image")[0].style.backgroundImage = `url(${images[Math.floor(Math.random() * 4)]})`;
@@ -126,15 +148,20 @@ bg_change();
 
 
 
+fetch('./data/data.json')
+.then(response => response.json())
+.then(json => { 
+    let x = json.filter((item) => { return item.name == "Amber";});
 
+    // if(x[0] != null)
+        
+    //     console.log(x[0].build_type);
+
+    // for (let key in x[0].build_type)
+    //     console.log(key);
+
+});
 
 
 // console.log(json);
 // console.log(json.filter((item) => { return item.id == 2;}));
-
-
-
-
-
-
-
